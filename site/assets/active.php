4e8b4c9910ed4b313fe19a7a0f9f9a21022da9e3
Michael Daldini <michael.daldini@gmail.com>
2018-12-21 10:53:58 +0100

    workaround to update git

---
+++ site/assets/active.php
@@ -0,0 +1,1 @@
+<?php // Do not delete this file. The existence of this file indicates the site is confirmed active and first-time use errors may be suppressed. Installed at: [/home/u421004058/domains/sphi-test.hostingerapp.com/public_html/]
